6bf3ce5444c01c284340ca4fbba26ba7b8db193c
Antares <enteringvalhalla@gmail.com>
2022-07-21 05:02:34 +0100

    Solve grid creation logic

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
-const container = document.getElementById("grid");
-let cell = document.createElement("div");
-cell.innerHTML = "TEXT";
-container.appendChild(cell); 
+function newGrid() {
+    const gridContainer = document.getElementById("container");
+
+    x = prompt("How many rows and columns do you want?");
+    console.log(x);
+
+    for (let i = 0; i < x; i++) {
+        let myRows = document.createElement("tr");
+        myRows.id = "row" + i;
+
+        gridContainer.appendChild(myRows);
+        let rowUnit = document.getElementById("row" + i);
+
+        for (let j = 1; j < x; j++) {
+            let myCells = document.createElement("td");
+            rowUnit.appendChild(myCells);
+        }
+    }
+}
+
+const setGrid = document.getElementById("set");
+setGrid.addEventListener("click", () => newGrid());
+
+document.addEventListener("DOMContentLoaded", () => newGrid());
