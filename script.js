3eb19f1f1ef204ab43083815715780bf3886d5c3
Antares <enteringvalhalla@gmail.com>
2022-08-17 01:54:11 +0100

    Finish working version with buttons and color randomizer

--- script.js
+++ script.js
@@ -1,24 +1,43 @@
+const setGrid = document.getElementById("set");
+const setRainbow = document.getElementById("rainbow");
+const setBlack = document.getElementById("black");
+let squareColor = false;
+
 function newGrid() {
     const gridContainer = document.getElementById("container");
-
-    x = prompt("How many rows and columns do you want?");
-    console.log(x);
-
-    for (let i = 0; i < x; i++) {
-        let myRows = document.createElement("tr");
-        myRows.id = "row" + i;
-
-        gridContainer.appendChild(myRows);
-        let rowUnit = document.getElementById("row" + i);
-
-        for (let j = 1; j < x; j++) {
-            let myCells = document.createElement("td");
-            rowUnit.appendChild(myCells);
+    gridContainer.innerHTML = '';
+    let x = prompt("How many squares per side do you want?");
+      if (x > 100) {
+        alert("Too many! 100 is the limit.");
+        newGrid();
+      }
+      else {
+        for (let i = 0; i < x ** 2; i++) {
+         const square = document.createElement("div");
+         square.classList.add('square');
+         gridContainer.appendChild(square);
+         gridContainer.style.setProperty('grid-template-columns', `repeat(${x}, auto)`);
+         gridContainer.style.setProperty('grid-template-rows', `repeat(${x}, auto)`);
+         square.addEventListener("mouseover", () => squarePaint(square));
         }
-    }
+  }
 }
 
-const setGrid = document.getElementById("set");
 setGrid.addEventListener("click", () => newGrid());
+setRainbow.addEventListener("click", function() { squareColor = true; });
+setBlack.addEventListener("click", function() { squareColor = false; });
+
+function squarePaint(square) {
+  let r = Math.floor(Math.random() * 256);
+  let g = Math.floor(Math.random() * 256);
+  let b = Math.floor(Math.random() * 256);
+
+  if (squareColor === false) {
+    square.style.setProperty('background-color', 'black');
+  }
+  else {
+    square.style.setProperty('background-color', `rgb(${r},${g},${b})`);
+  }
+}
 
 document.addEventListener("DOMContentLoaded", () => newGrid());
